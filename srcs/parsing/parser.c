9859d8cd2b927400831929faa2c06ea5c7add372
Geraud T'Serstevens <gt-serst@shi-c2-p6.s19.be>
2023-10-18 18:24:51 +0200

    ast pseudo code

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -6,29 +6,46 @@
 /*   By: gt-serst <gt-serst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 13:56:19 by gt-serst          #+#    #+#             */
-/*   Updated: 2023/10/17 17:22:30 by gt-serst         ###   ########.fr       */
+/*   Updated: 2023/10/18 18:20:19 by gt-serst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-t_node	*ast_new()
-{
-	//algo that will build the ast
-	//if (is_pipe())
-		//doing smthg
-	//else
-		//get_simple_cmd();
-	//get_children();
+/*
+	atoms are cmds and paranthesis in our case, operators are pipes
+*/
+
+t_node	*recursive_ast(int min_prec)
+{//give the priority for each elements in the ast according to a precedence climbing algorithm
+	//atom_lhs = compute_atom() gets the left atom
+	//get_next_token()
+	//while (is_pipe() && precedence >= min_prec)
+	//	if left assoc
+	//		next_min_prec = prec + 1
+	//	else
+	//		next_min_prec = prec
+	//	get_next_token()
+	//	atom_rhs = recursive_ast(next_min_prec)
+	//	atom_lhs = compute_operator(op, atom_lhs, atom_rhs) performs the arithmetic computation
+	//return atom_lhs
 }
 
-t_node	*get_children(t_token_type op, t_node *left, t_node *right)
+t_node	*compute_operator(t_token_type op, t_node *left, t_node *right)
 {// get the left and right children of the parent node
+	//if (is_pipe())
+	//	return left | right
+	//else
+	//	return parse_error("unknown operator %s", op)
+}
+
+t_node	*compute_atom(void)
+{// It handles true atoms (commands in our case), as well as parenthesized sub-expressions
 }
 
 t_node	*parser(void)
 {
 	//get first token from tokens list
-	//ast_builder();
+	//recursive_ast();
 	//return ast or parser error
 }
--- srcs/parsing/parser_err.c
+++ srcs/parsing/parser_err.c
@@ -6,12 +6,12 @@
 /*   By: gt-serst <gt-serst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 14:33:56 by gt-serst          #+#    #+#             */
-/*   Updated: 2023/10/17 17:22:33 by gt-serst         ###   ########.fr       */
+/*   Updated: 2023/10/18 18:21:28 by gt-serst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	handle_parsing_err(void)
+void	parse_error(void)
 {// determine the reason of the error and return the correct err code to parser function
 }
