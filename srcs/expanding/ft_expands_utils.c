8de4dfe20cf1f190229bb6cc443463e4fe54173f
Maxime De plaen <mde-plae@mi-r8-p1.s19.be>
2023-10-30 13:46:28 +0100

    bijna_gedaan

---
+++ srcs/expanding/ft_expands_utils.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_expands_utils.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-plae <mde-plae@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 10:04:10 by mde-plae          #+#    #+#             */
+/*   Updated: 2023/10/30 11:22:24 by mde-plae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+char	*ft_handle_dquote_str(char *str, size_t *i)
+{
+	size_t	start;
+
+	start = *i;
+	while (str[*i] != '"' && str[*i] != '$')
+	{
+		(*i)++;
+	}
+	return (ft_substr(str, start, *i - start));
+}
+
+char	*ft_handle_squotes(char *str, size_t *i)
+{
+	size_t	start;
+
+	start = *i;
+	(*i)++;
+	while (str[*i] != '\'')
+		(*i)++;
+	(*i)++;
+	return (ft_substr(str, start, *i - start));
+}
+
+char	*ft_handle_normal_str(char *str, size_t *i)
+{
+	size_t	start;
+
+	start = *i;
+	while (str[*i] && str[*i] != '\'' && str[*i] != '"' && str[*i] != '$')
+		(*i)++;
+	return (ft_substr(str, start, *i - start));
+}
+
+bool	ft_is_valid_var_char(char c)
+{
+	if (ft_isalnum(c) || c == '_')
+		return (true);
+	return (false);
+}
